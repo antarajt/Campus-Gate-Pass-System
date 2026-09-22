f0338397bfac3ae8e28c5bd15a81372ea7839cbe
Antar Chandra Das <antordas1435@gmail.co>
2026-09-22 15:05:35 +0000

    Initial campus gate pass Project

---
+++ static/js/app.js
@@ -0,0 +1,9 @@
+/* =========================================
+   Campus Gate Pass - Main JavaScript
+========================================= */
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("Campus Gate Pass loaded successfully.");
+
+});
